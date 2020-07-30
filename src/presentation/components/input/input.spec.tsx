96ac068f8dbfceac0ddbcdc5f0e71bbbf6beffad
vitoraa <vitor.lima@outlook.com>
2020-07-30 17:27:45 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/components/input/input.spec.tsx
+++ src/presentation/components/input/input.spec.tsx
@@ -1,16 +1,27 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult, fireEvent } from '@testing-library/react'
 import Input from './input'
 import Context from '@/presentation/context/form/form-context'
 
+const makeSut = (): RenderResult => {
+  return render(
+    <Context.Provider value={{ state: {} }}>
+      <Input name='field' />
+    </Context.Provider >
+  )
+}
+
 describe('Input Component', () => {
   test('Should call axios with correct values', () => {
-    const { getByTestId } = render(
-      <Context.Provider value={{ state: {} }}>
-        <Input name='field' />
-      </Context.Provider >
-    )
+    const { getByTestId } = makeSut()
     const input = getByTestId('field') as HTMLInputElement
     expect(input.readOnly).toBe(true)
+  })
+
+  test('Should remove readOnly on focus', () => {
+    const { getByTestId } = makeSut()
+    const input = getByTestId('field') as HTMLInputElement
+    fireEvent.focus(input)
+    expect(input.readOnly).toBe(false)
   })
 })

3c9199bcb788a91f2fc28492b62e4a890272c8d9
vitoraa <vitor.lima@outlook.com>
2020-08-03 16:03:32 -0300

    fix: adjust text Credenciais inválidas

--- src/domain/errors/invalid-credentials-error.ts
+++ src/domain/errors/invalid-credentials-error.ts
@@ -1,6 +1,6 @@
 export class InvalidCredentialsError extends Error {
   constructor () {
-    super('Credencias inválidas')
+    super('Credenciais inválidas')
     this.name = 'InvalidCredentialsError'
   }
 }

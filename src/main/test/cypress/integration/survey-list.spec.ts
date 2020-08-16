7b37d1ee11118c27e36cc9447748b8874a5b6a0f
vitoraa <vitor.lima@outlook.com>
2020-08-16 09:55:18 -0300

    test: ensure SurveyList presebt error on UnexpectedError

---
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -0,0 +1,16 @@
+import faker from 'faker'
+import * as Helper from '../support/helpers'
+import * as Http from '../support/survey-list-mocks'
+
+describe('SurveyList', () => {
+  beforeEach(() => {
+    Helper.setLocalStorageItem('account',
+      { accessToken: faker.random.uuid(), name: faker.name.findName() })
+  })
+
+  it('Should present error on UnexpectedError', () => {
+    Http.mockUnexpectedError()
+    cy.visit('')
+    cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
+  })
+})

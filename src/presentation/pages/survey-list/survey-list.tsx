932b952dcf393f5eaa199b42c465dfeb593f2226
vitoraa <vitor.lima@outlook.com>
2020-08-20 15:14:20 -0300

    refactor: move Error  to its own component

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Styles from './survey-list-styles.scss'
-import { Footer, Header } from '@/presentation/components'
-import { SurveyContext, SurveyListItem, ErrorSurveyListItem } from './components'
+import { Footer, Header, Error } from '@/presentation/components'
+import { SurveyContext, SurveyListItem } from './components'
 import { LoadSurveyList } from '@/domain/usecases'
 import { useErrorHandler } from '@/presentation/hooks'
 
@@ -20,6 +20,10 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
     reload: false
   })
 
+  const reload = (): void => {
+    setState(old => ({ surveys: [], error: '', reload: !old.reload }))
+  }
+
   useEffect(() => {
     loadSurveyList.loadAll()
       .then(surveys => setState(old => ({ ...old, surveys })))
@@ -32,7 +36,9 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
         <SurveyContext.Provider value={{ state, setState }}>
-          {state.error ? <ErrorSurveyListItem /> : <SurveyListItem />}
+          {state.error
+            ? <Error error={state.error} reload={reload} />
+            : <SurveyListItem />}
         </SurveyContext.Provider>
       </div>
       <Footer />

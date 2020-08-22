bfc5ff45f11c947b17247f510abcb234b9517c2f
vitoraa <vitor.lima@outlook.com>
2020-08-22 14:57:28 -0300

    test: ensure SurveyResult calls LoadSurveyResult

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -1,21 +1,33 @@
 import { SurveyResult } from '@/presentation/pages'
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import { ApiContext } from '@/presentation/context'
-import { mockAccountModel } from '@/domain/test'
+import { mockAccountModel, LoadSurveyResultSpy } from '@/domain/test'
 
-const makeSut = (): void => {
+type SutTypes = {
+  loadSurveyResultSpy: LoadSurveyResultSpy
+}
+
+const makeSut = (loadSurveyResultSpy = new LoadSurveyResultSpy()): SutTypes => {
   render(<ApiContext.Provider value={{ setCurrentAccount: jest.fn(), getCurrentAccount: () => mockAccountModel() }}>
-    < SurveyResult />
+    < SurveyResult loadSurveyResult={loadSurveyResultSpy} />
   </ApiContext.Provider>)
+  return { loadSurveyResultSpy }
 }
 
 describe('Survey Result Component', () => {
-  test('Should present correct initial state', () => {
+  test('Should present correct initial state', async () => {
     makeSut()
     const surveyResult = screen.getByTestId('survey-result')
     expect(surveyResult.childElementCount).toBe(0)
     expect(screen.queryByTestId('error')).not.toBeInTheDocument()
     expect(screen.queryByTestId('loading')).not.toBeInTheDocument()
+    await waitFor(() => surveyResult)
+  })
+
+  test('Should call LoadSurveyList', async () => {
+    const { loadSurveyResultSpy } = makeSut()
+    await waitFor(() => screen.getByTestId('survey-result'))
+    expect(loadSurveyResultSpy.callsCount).toBe(1)
   })
 })

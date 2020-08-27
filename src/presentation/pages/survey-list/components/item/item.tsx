f45b698e4045e90c4503207646c0da15e9a14967
vitoraa <vitor.lima@outlook.com>
2020-08-27 11:08:06 -0300

    feat: ensure SurveyList redirect to SurveyResult

--- src/presentation/pages/survey-list/components/item/item.tsx
+++ src/presentation/pages/survey-list/components/item/item.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Styles from './item-styles.scss'
 import { IconName, Icon, Calendar } from '@/presentation/components'
 import { LoadSurveyList } from '@/domain/usecases'
+import { Link } from 'react-router-dom'
 
 type Props = {
   survey: LoadSurveyList.Model
@@ -17,7 +18,7 @@ const SurveyItem: React.FC<Props> = ({ survey }: Props) => {
         <Calendar date={survey.date} className={Styles.calendarWrap} />
         <p data-testid="question">{survey.question}</p>
       </div>
-      <footer>Ver Resultado</footer>
+      <footer><Link data-testid="link" to={`/surveys/${survey.id}`}>Ver Resultado</Link></footer>
     </li >
   )
 }

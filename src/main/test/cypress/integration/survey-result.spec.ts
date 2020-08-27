d7b41b179fe14a4ee8cd13edecc28977b23ab7d4
vitoraa <vitor.lima@outlook.com>
2020-08-27 13:59:55 -0300

    test: ensure SurveyResult presents error if request fails

---
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -0,0 +1,29 @@
+import * as Helper from '../utils/helpers'
+import * as Http from '../utils/http-mocks'
+
+const path = /surveys/
+const mockUnexpectedError = (): void => Http.mockServerError(path, 'GET')
+const mockSuccess = (): void => Http.mockOk(path, 'GET', 'fx:survey-list')
+
+describe('SurveyResult', () => {
+  beforeEach(() => {
+    cy.fixture('account').then(account => {
+      Helper.setLocalStorageItem('account', account)
+    })
+  })
+
+  it('Should present error on UnexpectedError', () => {
+    mockUnexpectedError()
+    cy.visit('/surveys/any_id')
+    cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
+  })
+
+  it('Should reload on button click', () => {
+    mockUnexpectedError()
+    cy.visit('')
+    cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
+    mockSuccess()
+    cy.getByTestId('reload').click()
+    cy.get('li:not(:empty)').should('have.length', 2)
+  })
+})
